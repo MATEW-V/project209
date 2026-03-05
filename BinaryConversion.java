470678dff4485e6529967106fb94388fe66c436c
Matthew Vuong <matthewevuong@gmail.com>
2026-03-05 09:34:44 -0500

    more prog

--- BinaryConversion.java
+++ BinaryConversion.java
@@ -2,27 +2,28 @@ public class BinaryConversion {
     private String binary;
     private boolean isSigned; 
     
-    // default contructor: unsigned
+    // default constructor: unsigned
     public BinaryConversion(String binary) {
         if (!isBinary(binary)) {
             throw new IllegalArgumentException("not binary");
         }
+        if (binary.length() > 32) {
+            throw new IllegalArgumentException("binary string cannot exceed 32 bits");
+        }
         this.binary = binary;
         this.isSigned = false;
     }
-
-    // overloaded constructor with sign and magnitude
+    // overloaded constructor but signed
     public BinaryConversion(String binary, boolean signed) {
         if (!isBinary(binary)) {
             throw new IllegalArgumentException("invalid binary string");
         }
+        if (binary.length() > 32) {
+            throw new IllegalArgumentException("binary string cannot exceed 32 bits");
+        }
         this.binary = binary;
         this.isSigned = signed;
     }
-    
-    public void setSigned(boolean signed) { this.isSigned = signed; } // sign bool setter
-
-    public boolean isSigned() { return isSigned; }  // sign checker
     
     // signed decimal conv
     public long toDecimal() {
@@ -49,15 +50,13 @@ public class BinaryConversion {
             return value - (1L << binary.length());
         }
     }
-    //hex conversion
-    public String toHex() {
-        return Long.toHexString(toDecimal()).toUpperCase();
-    }
     
-    public int getDegree() {
-        return binary.length();
-    }
-    
+    public String toHex() { return Long.toHexString(toDecimal()).toUpperCase(); }   //hex conversion
+    public String getBinary() { return this.binary;}                                //basic get binary no formatting
+    public int getDegree() { return binary.length(); }                              //for floating points
+    public void setSigned(boolean signed) { this.isSigned = signed; }               // sign bool setter
+    public boolean isSigned() { return isSigned; }                                  // sign checker
+
     // set binary (preserves current sign setting)
     public void setBinary(String binary) {
         if (!isBinary(binary)) {
@@ -75,4 +74,27 @@ public class BinaryConversion {
         }
         return true;
     }
+
+   @Override
+    public String toString() { 
+        String extended32 = this.binary;
+        
+        if (extended32.length() < 32) {
+            char padChar = isSigned ? extended32.charAt(0) : '0';
+            while (extended32.length() < 32) {
+                extended32 = padChar + extended32;
+            }
+        }
+        // spaces every 4 bits
+        String res = "";
+        for (int i = 0; i < 32; i++) {
+            if (i > 0 && i % 4 == 0) {
+                res += " ";
+            }
+            res += extended32.charAt(i);
+        }
+        
+        return res;
+    }
+
 }
